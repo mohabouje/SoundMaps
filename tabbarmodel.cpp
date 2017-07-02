fc660d7af2cdbbd2a6ab8148a5b304f45b157456
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-07-02 15:21:13 +0200

    TabBarItem should be a QObject element,

--- tabbarmodel.cpp
+++ tabbarmodel.cpp
@@ -2,18 +2,21 @@
 void TabBarItem::setName(const QString &name) {
     if (_name != name) {
         _name = name;
+        emit nameChanged();
     }
 }
 
 void TabBarItem::setIcon(const QString &icon) {
     if (_icon != icon) {
         _icon = icon;
+        emit iconChanged();
     }
 }
 
 void TabBarItem::setCounter(int counter) {
     if (_counter != counter) {
         _counter = counter;
+        emit counterChanged();
     }
 }
 
@@ -47,27 +50,23 @@ QVariant TabBarModel::data(const QModelIndex &index, int role) const {
         return QVariant();
     }
 
-    TabBarItem item = _tabBarItems.at(row);
+    TabBarItem* item = _tabBarItems.at(row);
     switch (role) {
     case NameRole:
-        return QVariant::fromValue<QString>(item.name());
+        return QVariant::fromValue<QString>(item->name());
     case IconRole:
-        return QVariant::fromValue<QString>(item.icon());
+        return QVariant::fromValue<QString>(item->icon());
     case CounterRole:
-        return QVariant::fromValue<int>(item.counter());
+        return QVariant::fromValue<int>(item->counter());
     default:
         return QVariant();
     }
 }
 
-void TabBarModel::appendTab(const TabBarItem &item) {
-    _tabBarItems.append(item);
-}
-
 void TabBarModel::appendTab(const QString &name, const QString &icon, int counter) {
-    TabBarItem item;
-    item.setName(name);
-    item.setIcon(icon);
-    item.setCounter(counter);
+    TabBarItem* item = new TabBarItem(this);
+    item->setName(name);
+    item->setIcon(icon);
+    item->setCounter(counter);
     _tabBarItems.append(item);
 }
--- tabbarmodel.h
+++ tabbarmodel.h
@@ -3,14 +3,23 @@
 
 #include "config.h"
 #include <QAbstractListModel>
-class TabBarItem  {
+class TabBarItem : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(QString icon READ icon WRITE setIcon NOTIFY iconChanged)
+    Q_PROPERTY(int counter READ counter WRITE setCounter NOTIFY counterChanged)
 public:
+    explicit TabBarItem(QObject* parent = Q_NULLPTR) : QObject(parent) {}
     inline QString name() const { return _name; }
     inline QString icon() const { return _icon; }
     inline int counter() const {  return _counter; }
     void setName(const QString &name);
     void setIcon(const QString &icon);
     void setCounter(int counter);
+signals:
+    void nameChanged();
+    void iconChanged();
+    void counterChanged();
 private:
     QString _name;
     QString _icon;
@@ -21,14 +30,13 @@ private:
 class TabBarModel : public QAbstractListModel {
     Q_OBJECT
 public:
-    explicit TabBarModel(QObject* parent = nullptr);
-    Q_INVOKABLE void appendTab(const TabBarItem& item);
+    explicit TabBarModel(QObject* parent = Q_NULLPTR);
     Q_INVOKABLE void appendTab(const QString& name, const QString& icon, int counter = 0);
     enum TabBarRoles { NameRole = Qt::UserRole + 1, IconRole, CounterRole };
     QHash<int, QByteArray> roleNames() const override;
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role) const override;
 private:
-    QList<TabBarItem>   _tabBarItems;
+    QList<TabBarItem*>   _tabBarItems;
 };
 #endif // TABBARMODEL_H
